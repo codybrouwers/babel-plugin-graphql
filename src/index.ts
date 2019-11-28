f4f6210f6987b5aef83c0817fea44947f222fb3e
Cody Brouwers <clbrouwers@gmail.com>
2019-11-29 04:13:40 +0900

    Print out query for component

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 /* eslint-disable no-console */
 import jsx from "@babel/plugin-syntax-jsx";
 import { PluginObj } from "@babel/core";
-import babelTypes from "@babel/types";
+import template from "@babel/template";
+import babelTypes, { templateElement } from "@babel/types";
 import { print } from "graphql";
-import {
-  isUseQuery,
-  getDataIdentifier,
-  getQueryName,
-  getQueryType,
-  graphqlAST,
-} from "./utils";
+import { isUseQuery, getDataIdentifier, getQueryName, getQueryType, graphqlAST } from "./utils";
 import { GraphQLPathParser } from "./GraphQLPathParser";
 
 export default function({ types: t }: { types: typeof babelTypes }): PluginObj {
@@ -29,7 +24,6 @@ export default function({ types: t }: { types: typeof babelTypes }): PluginObj {
 
         const querySelections = [queryType].map((parentType) => {
           const { referencePaths } = path.scope.bindings[dataIdentifier] || {};
-
           return referencePaths.reduce((queryFieldNode, referencePath) => {
             if (referencePath.node.type !== "Identifier") return queryFieldNode;
             if (referencePath.parent.type !== "MemberExpression") return queryFieldNode;
@@ -43,7 +37,35 @@ export default function({ types: t }: { types: typeof babelTypes }): PluginObj {
         // console.log("--querySelections", querySelections);
         const documentNode = graphqlAST.newDocumentNode(queryName, querySelections);
         // console.log(JSON.stringify(documentNode, null, 2));
-        console.log(print(documentNode));
+        const printedQuery = print(documentNode);
+        // console.log(printedQuery);
+
+        const queryTemplate = template`
+          COMPONENT_NAME = gqlQUERY;
+        `;
+
+        // const ast = babelTypes.templateLiteral(
+        //   [babelTypes.templateElement("/user", false), babelTypes.templateElement("", true)],
+        //   [babelTypes.identifier("id")]
+        // );
+        const templateExpression = t.taggedTemplateExpression(
+          t.identifier("gql"),
+          t.templateLiteral(
+            [t.templateElement({ raw: printedQuery, cooked: printedQuery }, false)],
+            []
+          )
+        );
+        const queryDeclaration = t.variableDeclaration("const", [
+          t.variableDeclarator(t.identifier("MOVIE_QUERY"), templateExpression),
+        ]);
+        // console.log(thing);
+        // const queryAST = queryTemplate({
+        //   COMPONENT_NAME: t.identifier("MOVIE_QUERY"),
+        //   QUERY: t.templateElement({ raw: printedQuery, cooked: printedQuery }),
+        // });
+        // console.log(queryAST);
+        const parent = path.getFunctionParent();
+        parent.insertBefore(queryDeclaration);
       },
     },
   };
