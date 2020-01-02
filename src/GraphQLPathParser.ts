bb5acdda6b21a4259b358a54be0a6ea2204a817f
Cody Brouwers <clbrouwers@gmail.com>
2020-01-02 16:43:49 +0900

    Update GraphQLPathParser.ts

--- src/GraphQLPathParser.ts
+++ src/GraphQLPathParser.ts
@@ -1,4 +1,4 @@
-import { MemberExpression, Node } from "@babel/types";
+import { MemberExpression, Node, CallExpression } from "@babel/types";
 import { Kind, FieldNode } from "graphql";
 import { NodePath } from "@babel/core";
 import { graphqlAST, callExpressionArguments } from "./utils";
@@ -9,11 +9,26 @@ import { graphqlAST, callExpressionArguments } from "./utils";
 
 // == Functions ============================================================
 
-function findOrCreateFieldNode(fieldNode: FieldNode, propertyName: string, parent: Node) {
-  return (
-    findFieldNode(fieldNode, propertyName) ||
-    graphqlAST.newFieldNode(propertyName, callExpressionArguments(parent))
-  );
+function findOrCreateFieldNode(
+  fieldNode: FieldNode,
+  propertyName: string,
+  argumentsNode: CallExpression | null
+) {
+  const existingFieldNode = findFieldNode(fieldNode, propertyName);
+  const options = argumentsNode ? callExpressionArguments(argumentsNode) : undefined;
+  if (existingFieldNode) {
+    mergeCallExpressionArguments(existingFieldNode, options);
+    return existingFieldNode;
+  }
+  return graphqlAST.newFieldNode(propertyName, options);
+}
+
+function mergeCallExpressionArguments(
+  fieldNode: $Writeable<FieldNode>,
+  { argumentNodes = [], directiveNodes = [] }: ReturnType<typeof callExpressionArguments> = {}
+) {
+  fieldNode.arguments = [...(fieldNode.arguments || []), ...argumentNodes];
+  fieldNode.directives = [...(fieldNode.directives || []), ...directiveNodes];
 }
 
 function findFieldNode(fieldNode: FieldNode, propertyName: string) {
@@ -30,28 +45,47 @@ function filterFieldNodesWithoutProperty(fieldNode: FieldNode, propertyName: str
   );
 }
 
-function getParentPropertyName({ object }: MemberExpression): string | null {
-  switch (object.type) {
-    case "Identifier":
-      return object.name;
-    case "MemberExpression":
-      return object.property.name;
-    default:
-      return null;
+function getParentPropertyName(node: MemberExpression | CallExpression): string | null {
+  if (node.type === "MemberExpression") {
+    const { object } = node;
+    switch (object.type) {
+      case "Identifier":
+        return object.name;
+      case "MemberExpression":
+        return object.property.name;
+      default:
+        return null;
+    }
+  } else if (node.type === "CallExpression" && node.callee.type === "Identifier") {
+    return node.callee.name;
   }
+  return null;
+}
+
+function getArgumentsNode(path: NodePath<MemberExpression | CallExpression>) {
+  if (path.isMemberExpression() && path.parent.type === "CallExpression") return path.parent;
+  if (path.isCallExpression()) return path.node;
+  return null;
 }
 
 function addFieldNodeForPathNode(
-  node: MemberExpression,
-  parent: Node,
+  path: NodePath<MemberExpression | CallExpression>,
   fieldNode: $Writeable<FieldNode>
 ): FieldNode {
-  const propertyName = node.property.name;
-  const propertyFieldNode = findOrCreateFieldNode(fieldNode, propertyName, parent);
-  const existingFieldNodes = filterFieldNodesWithoutProperty(fieldNode, propertyName);
+  const { node } = path;
+  const propertyName = nodeName(node);
+  if (!propertyName) return fieldNode;
 
+  const propertyFieldNode = findOrCreateFieldNode(fieldNode, propertyName, getArgumentsNode(path));
+  const existingFieldNodes = filterFieldNodesWithoutProperty(fieldNode, propertyName);
   fieldNode.selectionSet = graphqlAST.newSelectionSet([...existingFieldNodes, propertyFieldNode]);
   return propertyFieldNode;
+}
+
+function nodeName(node: MemberExpression | CallExpression) {
+  if (node.type === "MemberExpression") return node.property.name;
+  if (node.type === "CallExpression" && node.callee.type === "Identifier") return node.callee.name;
+  return null;
 }
 
 // == Exports ==============================================================
@@ -73,14 +107,13 @@ export class GraphQLPathParser {
 
   addNodes() {
     const ancestors = this.path.getAncestry();
-    for (const { node, parent } of ancestors.slice(1)) {
-      if (node.type !== "MemberExpression") continue;
-
-      if (this.dataIdentifier === getParentPropertyName(node)) {
-        this.parentFieldNode = addFieldNodeForPathNode(node, parent, this.queryFieldNode);
+    for (const ancestorPath of ancestors.slice(1)) {
+      if (!ancestorPath.isMemberExpression() && !ancestorPath.isCallExpression()) continue;
+      if (this.dataIdentifier === getParentPropertyName(ancestorPath.node)) {
+        this.parentFieldNode = addFieldNodeForPathNode(ancestorPath, this.queryFieldNode);
       } else {
         if (!this.parentFieldNode) return;
-        this.parentFieldNode = addFieldNodeForPathNode(node, parent, this.parentFieldNode);
+        this.parentFieldNode = addFieldNodeForPathNode(ancestorPath, this.parentFieldNode);
       }
     }
   }
