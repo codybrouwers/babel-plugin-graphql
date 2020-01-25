9e4879ff3b14e8420a1afe1998f48fe8a0812f6d
Cody Brouwers <clbrouwers@gmail.com>
2020-01-26 02:46:37 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
-    plugins: ["@babel/plugin-proposal-optional-chaining"],
   };
 };
