b3678b6015deb6edd0f74cba048487ca89a4a10a
Cody Brouwers <clbrouwers@gmail.com>
2020-01-27 20:57:08 +0900

    Setup next test

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -5,47 +5,49 @@ import plugin from "..";
 describe("tests pass", () => {
   pluginTester({
     plugin,
-    fixtures: path.join(__dirname, "./fixtures"),
+    // fixtures: path.join(__dirname, "./fixtures"),
     tests: [
-      // {
-      //   code: `
-      //     function Movie() {
-      //       const { data } = useQuery("GetMovie");
-      //       const { day } = data.year;
-      //       const theYear = data.year;
-      //       return (
-      //         <div>
-      //           <div>
-      //             <p>{data.id}</p>
-      //             <p>{theYear}</p>
-      //           </div>
-      //         </div>
-      //       );
-      //     }
-      // `,
-      //   output: `
-      //     const _MOVIE__GETMOVIEQUERY = gql\`
-      //       query Movie__GetMovieQuery {
-      //         GetMovie {
-      //           year {
-      //             month
-      //           }
-      //         }
-      //       }
-      //     \`;
-      //     function Movie() {
-      //       const { data } = useQuery(_MOVIE__GETMOVIEQUERY);
-      //       const { month } = data.year;
-      //       return (
-      //         <div>
-      //           <div>
-      //             <p>{month}</p>
-      //           </div>
-      //         </div>
-      //       );
-      //     }
-      // `,
-      // },
+      {
+        code: `
+          function Movie() {
+            const { data } = useQuery("GetMovie");
+            const release = data.releaseDate;
+            return (
+              <div>
+                <div>
+                  <p>{data.id}</p>
+                  <p>{release.more}</p>
+                </div>
+              </div>
+            );
+          }
+      `,
+        output: `
+          const _MOVIE__GETMOVIEQUERY = gql\`
+            query Movie__GetMovieQuery {
+              GetMovie {
+                releaseDate {
+                  more
+                }
+                id
+              }
+            }
+          \`;
+
+          function Movie() {
+            const { data } = useQuery(_MOVIE__GETMOVIEQUERY);
+            const release = data.releaseDate;
+            return (
+              <div>
+                <div>
+                  <p>{data.id}</p>
+                  <p>{release.more}</p>
+                </div>
+              </div>
+            );
+          }
+      `,
+      },
     ],
   });
 });
